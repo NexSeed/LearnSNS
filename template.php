d9d60f0fecba9150dd73f449a0839d7c2dcf050d
Atsushi <>
2018-07-15 23:06:41 +0800

    signアップ画面モック作成

--- template.php
+++ template.php
@@ -3,20 +3,20 @@
 ?>
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>Learn SNS</title>
-        <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
-    </head>
-    <body style="margin-top: 60px">
-        <div class="container">
-            <div class="row">
-                <!-- ここにコンテンツ -->
-            </div>
+<head>
+    <meta charset="utf-8">
+    <title>Learn SNS</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
+</head>
+<body style="margin-top: 60px">
+    <div class="container">
+        <div class="row">
+            <!-- ここにコンテンツ -->
         </div>
-        <script src="../assets/js/jquery-3.1.1.js"></script>
-        <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
-        <script src="../assets/js/bootstrap.js"></script>
-    </body>
+    </div>
+    <script src="../assets/js/jquery-3.1.1.js"></script>
+    <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+    <script src="../assets/js/bootstrap.js"></script>
+</body>
 </html>

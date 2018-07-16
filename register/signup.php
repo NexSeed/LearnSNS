59416252a375d78e512bde7c78f223e11bb0202f
Atsushi <>
2018-07-16 22:52:43 +0800

    パスワードの文字数バリデーション追加

--- register/signup.php
+++ register/signup.php
@@ -15,8 +15,11 @@
             $errors['email'] = 'blank';
         }
 
+        $count = strlen($password);
         if ($password == '') {
             $errors['password'] = 'blank';
+        } elseif ($count < 4 || 16 < $count) {
+            $errors['password'] = 'length';
         }
 
     }
@@ -57,6 +60,9 @@
                         <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
                         <?php if(isset($errors['password']) && $errors['password'] == 'blank') : ?>
                             <p class="text-danger">パスワードを入力してください</p>
+                        <?php endif; ?>
+                        <?php if(isset($errors['password']) && $errors['password'] == 'length') : ?>
+                            <p class="text-danger">パスワードは4 ~ 16文字で入力してください</p>
                         <?php endif; ?>
                     </div>
                     <div class="form-group">

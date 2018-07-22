5935849a1e6f5e856a80ec97cf413d6f1a3d2d5e
Atsushi <atsushi@ikedaatBookpuro.local>
2018-07-22 16:23:19 +0800

    一覧表示機能

--- timeline.php
+++ timeline.php
@@ -50,9 +50,6 @@
         $feeds[] = $record;
     }
 
-    echo '<pre>';
-    var_dump($feeds);die();
-
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -120,40 +117,43 @@
                         <input type="submit" value="投稿する" class="btn btn-primary">
                     </form>
                 </div>
-                <div class="thumbnail">
-                    <div class="row">
-                        <div class="col-xs-1">
-                            <img src="https://placehold.jp/40x40.png" width="40">
+                <?php foreach($feeds as $feed){ ?>
+                    <div class="thumbnail">
+                        <div class="row">
+                            <div class="col-xs-1">
+                                <img src="user_profile_img/<?php echo $feed['img_name']; ?>" width="40">
+                            </div>
+                            <div class="col-xs-11">
+                                <?php echo $feed['name']; ?><br>
+                                <a href="#" style="color: #7F7F7F;"><?php echo $feed['created']; ?></a>
+                            </div>
                         </div>
-                        <div class="col-xs-11">
-                            野原ひろし<br>
-                            <a href="#" style="color: #7F7F7F;">2018-03-03</a>
+                        <div class="row feed_content">
+                            <div class="col-xs-12" >
+                                <span style="font-size: 24px;"><?php echo $feed['feed']; ?></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row feed_content">
-                        <div class="col-xs-12" >
-                            <span style="font-size: 24px;">夢は逃げない。逃げるのはいつも自分だ。</span>
-                        </div>
-                    </div>
-                    <div class="row feed_sub">
-                        <div class="col-xs-12">
-                            <form method="POST" action="" style="display: inline;">
-                                <input type="hidden" name="feed_id" >
+                        <div class="row feed_sub">
+                            <div class="col-xs-12">
+                                <form method="POST" action="" style="display: inline;">
+                                    <input type="hidden" name="feed_id" >
 
-                                <input type="hidden" name="like" value="like">
-                                <button type="submit" class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</button>
-                            </form>
-                            <span class="like_count">いいね数 : 100</span>
-                            <span class="comment_count">コメント数 : 9</span>
-                            <a href="#" class="btn btn-success btn-xs">編集</a>
-                            <a href="#" class="btn btn-danger btn-xs">削除</a>
+                                    <input type="hidden" name="like" value="like">
+                                    <button type="submit" class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</button>
+                                </form>
+                                <span class="like_count">いいね数 : 100</span>
+                                <span class="comment_count">コメント数 : 9</span>
+                                <a href="#" class="btn btn-success btn-xs">編集</a>
+                                <a href="#" class="btn btn-danger btn-xs">削除</a>
+                            </div>
                         </div>
                     </div>
+                <?php } ?>
                 </div>
                 <div aria-label="Page navigation">
                     <ul class="pager">
-                        <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Older</a></li>
-                        <li class="next"><a href="#">Newer <span aria-hidden="true">&rarr;</span></a></li>
+                        <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
+                        <li class="next"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
                     </ul>
                 </div>
             </div>

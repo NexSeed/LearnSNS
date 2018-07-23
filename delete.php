89eb9fed403b7909ef14d0a6c867d19ce1824aa2
Atsushi <atsushi@ikedaatsushinoMacBook-puro.local>
2018-07-23 17:58:40 +0800

    削除機能実装

--- delete.php
+++ delete.php
@@ -1,3 +1,15 @@
 <?php
 
-echo '削除';
+    require_once('dbconnect.php');
+
+    $feed_id = $_GET["feed_id"];
+
+    $sql ="DELETE FROM `feeds` WHERE `feeds`.`id` = ?";
+
+    $data = array($feed_id);
+
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    header("Location: timeline.php");
+    exit();

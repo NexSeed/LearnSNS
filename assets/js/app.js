c4f9b8ec2e1ff2c8a5643f49e67182007179c283
Atsushi <atsushi@ikedaatsushinoMacBook-puro.local>
2018-07-25 18:49:31 +0800

    いいね数を増やす実施

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,10 @@ $(function() {
     $('.js-like').on('click', function() {
         var feed_id = $(this).siblings('.feed-id').text();
         var user_id = $('#signin-user').text();
+        var like_btn = $(this);
+        var like_count = $(this).siblings('.like_count').text();
         console.log(feed_id);   //feed_idを取得できているか確認
         console.log(user_id);   //user_idを取得できているか確認
-
         $.ajax({
             // 送信先、送信するデータなど
             url: 'like.php',
@@ -17,11 +18,13 @@ $(function() {
             }
         })
         .done(function(data) {
-            console.log(data);
+            if (data == 'true') {
+                like_count++;
+                like_btn.siblings('.like_count').text(like_count);
+            }
         })
         .fail(function(err) {
             console.log('error');
         })
     });
-
 });

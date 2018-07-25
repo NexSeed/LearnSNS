a4d5276b783b06dbde4bdbc87927ad1e40afac07
Atsushi <atsushi@ikedaatsushinoMacBook-puro.local>
2018-07-25 15:04:50 +0800

    ajaxのテンプレート作成

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,15 @@
 $(function() {
 
     $('.js-like').on('click', function() {
-        console.log(2);
+        $.ajax({
+            // 送信先、送信するデータなど
+        })
+        .done(function(data) {
+            // 成功時の処理
+        })
+        .fail(function(err) {
+            // 失敗時の処理
+        })
     });
 
 });

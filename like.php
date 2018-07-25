664b03bddf6484e5e68d58e088119919ad94891c
Atsushi <atsushi@ikedaatsushinoMacBook-puro.local>
2018-07-25 22:44:46 +0800

    いいねを取り消すボタンを作成

--- like.php
+++ like.php
@@ -5,7 +5,11 @@
     $feed_id = $_POST["feed_id"];
     $user_id = $_POST["user_id"];
 
-    $sql = "INSERT INTO `likes` (`user_id`, `feed_id`) VALUES (?, ?);";
+    if (isset($_POST["is_unlike"])) {
+         $sql ="DELETE FROM `likes` WHERE `user_id` = ?  and `feed_id` = ?";
+    } else {
+         $sql = "INSERT INTO `likes` (`user_id`, `feed_id`) VALUES (?, ?);";
+    }
 
     $data = [$user_id, $feed_id];
     $stmt = $dbh->prepare($sql);
